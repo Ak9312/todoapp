fc862fd2b4b5dc3ba6bb7cd2d7ef366b6b12cf1e
Ak9312 <129245931+Ak9312@users.noreply.github.com>
2024-04-05 22:42:01 +0530

    Almost Completed

--- Script.js
+++ Script.js
@@ -1,6 +1,15 @@
 let todoList = [];
 const MAX_TODO_ITEMS = 6;
 let addTask = document.querySelector(".AddTask");
+let confirmDelete = document.querySelector(".DeleteConfirm");
+let deleteItem = document.querySelector(".DeleteBody")
+let deleteConfirmButton = document.querySelector(".ConfirmDelete")
+let deleteCancelButton = document.querySelector(".CancelDelete")
+let editItem = document.querySelector(".EditConfirm");
+let editValue = document.querySelector(".EditText");
+let editConfirmButton = document.querySelector(".ConfirmEdit")
+let editCancelButton = document.querySelector(".CancelEdit")
+
 
 function renderRowContainter() {
   // Clearing OutputContainer
@@ -26,8 +35,46 @@ function renderRowContainter() {
     rowContainer.appendChild(taskDisplayContainer);
 
     //Add Task Edit Button Container
+    const taskEdit = document.createElement("div");
 
+    taskEdit.classList.add("TaskEditButtonContainer");
+    const editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    editButton.id = "taskedit_" + `${index}`;
+    taskEdit.appendChild(editButton);
+    rowContainer.appendChild(taskEdit);
     //Add Task Delete Button Container
+    const taskDelete = document.createElement("div");
+
+    taskDelete.classList.add("TaskDeleteButtonContainer");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.id = "taskDelete_" + `${index}`;
+    taskDelete.appendChild(deleteButton);
+    rowContainer.appendChild(taskDelete);
+
+deleteButton.addEventListener("click", ()=> {
+  confirmDelete.style.display = "block";
+  deleteItem.textContent =  ` ${item}`;
+})
+
+deleteCancelButton.addEventListener("click", () => {
+  confirmDelete.style.display = "none";
+
+})
+
+editButton.addEventListener("click", () => {
+  editItem.style.display = "block";
+  editValue.value =  ` ${item}`;
+})
+
+
+editCancelButton.addEventListener("click", () => {
+  editItem.style.display = "none";
+
+})
+
+
   });
 }
 
@@ -45,6 +92,9 @@ function addTaskHandler() {
   }
   todoList.push(inputBox.value);
   renderRowContainter();
+  inputBox.value = "";
 }
 
 addTask.addEventListener("click", addTaskHandler);
+
+
